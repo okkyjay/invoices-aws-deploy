ab199f16dac19335b465dcedbd09aceae8e0f50e
Oke Ayodeji Anthony <okeayodeji.a@gmail.com>
2025-08-24 15:17:11 +0100

    Changed IP

--- src/app.js
+++ src/app.js
@@ -13,4 +13,4 @@ app.use(express.json());
 app.use("/api/invoices", require("./routes/invoice.routes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,  "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
